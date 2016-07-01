9e2ebbd37cec26af204298368a4c9dd5d97f2a98
Don Park <donp@donp.org>
2016-07-01 11:24:30 -0700

    exchanges verb

--- api.js
+++ api.js
@@ -9,17 +9,65 @@ let rethinkdb = require('rethinkdb')
 let config = JSON.parse(fs.readFileSync('./config.hjson'))
 console.log('config', config)
 
-websock.listen(config.websocket.listen_port, do_connect);
 
-function do_connect(socket) {
-  console.log('ws_connect')
+rethinkdb.connect({db: 'cointhink'})
+.then(function(conn){
+  rethinkdb
+  .tableList()
+  .run(conn)
+  .then(function(list){
+    console.log(list)
+  })
 
-  socket.on('message', function(data) {
-    console.log('<-ws '+data)
-  });
+  websock.listen(config.websocket.listen_port, function(socket) {
+    do_connect(socket, conn)
+  })
+
+})
+
+
+function do_connect(socket, db) {
+  console.log('websocket open')
+
+  socket.on('message', function(msg) {
+    var rpc = JSON.parse(msg)
+    console.log('<-ws '+ JSON.stringify(rpc))
+
+    if (rpc.method == "orderbook") {
+      return rethinkdb
+      .table('orderbooks')
+      .orderBy({index: rethinkdb.desc('date')})
+      .limit(1)
+      .run(db)
+      .then(function(cursor){
+        cursor
+        .toArray()
+        .then(function(books){
+          let book = books[0]
+          console.log(rpc.method, rpc.params, '->', book.date, book.market)
+          socket.send(JSON.stringify(book))
+        })
+      })
+    }
+
+    if (rpc.method == "exchanges") {
+      return rethinkdb
+      .table('exchanges')
+      .run(db)
+      .then(function(cursor){
+        cursor
+        .toArray()
+        .then(function(exchanges){
+          exchanges = exchanges.map(function(ex){return {name: ex.id}})
+          console.log(rpc.method, '->', exchanges)
+          socket.send(JSON.stringify(exchanges))
+        })
+      })
+    }
+  })
 
   socket.on('close', function() {
-    console.log('websockets close');
-  });
+    console.log('websocket close')
+  })
 }
 
