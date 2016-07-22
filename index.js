3ac628b264587d69a23a93e867ca4cd015909799
Don Park <donp@donp.org>
2016-07-22 13:44:10 -0700

    last exchange info, catch json err

--- index.js
+++ index.js
@@ -30,49 +30,66 @@ function do_connect(socket, db) {
   console.log('websocket open')
 
   socket.on('message', function(msg) {
-    var rpc = JSON.parse(msg)
-    console.log('<-ws '+ JSON.stringify(rpc))
-  //exchange: 'poloniex',
-  //market: { base: 'XMR', quote: 'XDN' },
-
-    if (rpc.method == "orderbook") {
-      let now = new Date()
-      let base = rpc.params.base.toUpperCase()
-      let quote = rpc.params.quote.toUpperCase()
-      let hours = parseInt(rpc.params.hours)
-      let early = [base, quote, new Date(now-1000*60*60*hours)]
-      let late = [base, quote, now]
-      return rethinkdb
-      .table('orderbooks')
-      .orderBy({index: rethinkdb.desc('base-quote-date')})
-      .between(early, late)
-      .run(db)
-      .then(function(cursor){
-        cursor
-        .each(function(err, book){
-          book.asks = [ book.asks[0] ]
-          book.bids = [ book.bids[0] ]
-          obsend('orderbook', book)
-        })
-      })
+    try {
+      var rpc = JSON.parse(msg)
+      console.log('<-ws', JSON.stringify(rpc))
+      dispatch(rpc)
+    } catch (e){
+      console.log('<-bad', msg)
     }
 
-    if (rpc.method == "exchanges") {
-      return rethinkdb
-      .table('exchanges')
-      .run(db)
-      .then(function(cursor){
-        cursor
-        .each(function(err, exchange){
-          obsend('exchange', exchange)
+    function dispatch(rpc) {
+      if (rpc.method == "orderbook") {
+        let now = new Date()
+        let base = rpc.params.base.toUpperCase()
+        let quote = rpc.params.quote.toUpperCase()
+        let hours = parseInt(rpc.params.hours)
+        let early = [base, quote, new Date(now-1000*60*60*hours)]
+        let late = [base, quote, now]
+        return rethinkdb
+        .table('orderbooks')
+        .orderBy({index: rethinkdb.desc('base-quote-date')})
+        .between(early, late)
+        .run(db)
+        .then(function(cursor){
+          cursor
+          .each(function(err, book){
+            book.asks = [ book.asks[0] ]
+            book.bids = [ book.bids[0] ]
+            obsend('orderbook', book)
+          })
         })
-      })
-    }
+      }
 
-    function obsend(type, object) {
-      let obj = { type: type, object: object}
-      console.log(rpc.method, rpc.params, '->', JSON.stringify(obj, null, 2))
-      socket.send(JSON.stringify(obj))
+      if (rpc.method == "exchanges") {
+        return rethinkdb
+        .table('exchanges')
+        .run(db)
+        .then(function(cursor){
+          cursor
+          .each(function(err, exchange){
+            return rethinkdb
+            .table('orderbooks')
+            .orderBy({index: rethinkdb.desc('exchange-date')})
+            .between([exchange.id, new Date(0)], [exchange.id, new Date()])
+            .limit(1)
+            .run(db)
+            .then(function(cursor){
+              cursor
+              .each(function(err, lastbook){
+                let exchangeStat = {exchange: exchange.id, lastQuote: lastbook.date}
+                obsend('exchange', exchangeStat)
+              })
+            })
+          })
+        })
+      }
+
+      function obsend(type, object) {
+        let obj = { type: type, object: object}
+        console.log(rpc.method, rpc.params, '->', JSON.stringify(obj, null, 2))
+        socket.send(JSON.stringify(obj))
+      }
     }
   })
 
